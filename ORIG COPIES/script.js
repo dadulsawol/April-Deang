eb05a8cbe4d468734aa55fd06b3d7acfb6350d4b
dadulsawol <apriljoydeang23@gmail.com>
2023-10-05 23:29:28 +0800

    Hamburger
    hamburger nav for tablet/mobile devices

---
+++ ORIG COPIES/script.js
@@ -0,0 +1,5 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav1").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav1").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-  }
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
