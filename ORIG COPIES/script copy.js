ea16f87743f4271dc778246e9512b7a9f19698ca
dadulsawol <apriljoydeang23@gmail.com>
2023-10-11 23:33:28 +0800

    Added gif
    Added some designs and fixed media

--- ORIG COPIES/script copy.js
+++
@@ -1,5 +0,0 @@
-function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav1").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-  }
