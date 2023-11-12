ba0df91078d07d35988df6e53a0173aee8f801a7
dadulsawol <apriljoydeang23@gmail.com>
2023-11-12 23:52:51 +0800

    changed my layout

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+const ToggleButton = document.querySelector('.ToggleButton')
+const DropdownMenu = document.querySelector('.DropdownMenu')
+
+ToggleButton.onclick = function () {
+  DropdownMenu.classList.toggle('open')
 }
+
+window.addEventListener('scroll', function() {
+  const navBar = document.getElementById('#NavBar');
+  if (window.pageYOffset > 0) {
+      navBar.style.position = 'fixed';
+      navBar.style.top = '0';
+  } else {
+      navBar.style.position = 'relative';
+  }
+});
